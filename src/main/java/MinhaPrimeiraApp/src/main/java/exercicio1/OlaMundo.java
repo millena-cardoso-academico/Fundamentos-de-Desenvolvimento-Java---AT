2fa4ecc033a2f56aca25b90820fa059b4f8112fc
Millena Cardoso <millena.santos@al.infnet.edu.br>
2025-03-31 16:50:37 -0300

    exercicio1
    correcao exercicio 1.

---
+++ src/main/java/MinhaPrimeiraApp/src/main/java/exercicio1/OlaMundo.java
@@ -0,0 +1,8 @@
+package exercicio1;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        String nome = "Millena Cardoso Sales Santos";
+        System.out.println("Olá, meu nome é " + nome + " e estou aprendendo Java!");
+    }
+}
